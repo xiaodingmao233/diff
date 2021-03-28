9f381043cfd17f3b290fd5ba51a90627a05fcbba
lizheng <2112276441@qq.com>
2021-03-28 15:46:01 +0800

    虚拟DOM和h函数

--- study-snabbdom/src/index.js
+++ study-snabbdom/src/index.js
@@ -5,31 +5,31 @@ import { styleModule } from 'snabbdom/modules/style'
 import { eventListenersModule } from 'snabbdom/modules/eventlisteners'
 import { h } from 'snabbdom/h' // helper function for creating vnodes
 
-const patch = init([ // Init patch function with chosen modules
-  classModule, // makes it easy to toggle classes
-  propsModule, // for setting properties on DOM elements
-  styleModule, // handles styling on elements with support for animations
-  eventListenersModule, // attaches event listeners
+// 创建出patch函数
+const patch = init([classModule, propsModule, styleModule, eventListenersModule])
+
+// 创建虚拟节点
+const myVnode1 = h('a', {
+  props: {
+    href: 'http://www.baidu.com',
+    target: '_blank'
+  }
+}, '百度')
+console.log(myVnode1)
+
+const myVnode2 = h('div', {
+  class: {
+    'box': true
+  }
+}, '我是一个盒子')
+
+const myVnode3 = h('ul', [
+  h('li', '苹果'),
+  h('li', '西瓜'),
+  h('li', '香蕉'),
+  h('li', '鸭梨')
 ])
 
+// 让虚拟节点上树
 const container = document.getElementById('container')
-
-const vnode = h('div#container.two.classes', { on: { click: function () {
-
-} } }, [
-  h('span', { style: { fontWeight: 'bold' } }, 'This is bold'),
-  ' and this is just normal text',
-  h('a', { props: { href: '/foo' } }, 'I\'ll take you places!')
-])
-// Patch into empty DOM element – this modifies the DOM as a side effect
-patch(container, vnode)
-
-const newVnode = h('div#container.two.classes', { on: { click: function () {
-  
-} } }, [
-  h('span', { style: { fontWeight: 'normal', fontStyle: 'italic' } }, 'This is now italic type'),
-  ' and this is still just normal text',
-  h('a', { props: { href: '/bar' } }, 'I\'ll take you places!')
-])
-// Second `patch` invocation
-patch(vnode, newVnode) // Snabbdom efficiently updates the old view to the new state
+patch(container, myVnode3)
