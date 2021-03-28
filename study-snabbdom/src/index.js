72a4931de4e0bef5bf62f94a7327453b48a1a459
lizheng <2112276441@qq.com>
2021-03-28 16:46:30 +0800

    手写h函数

--- study-snabbdom/src/index.js
+++ study-snabbdom/src/index.js
@@ -1,35 +1,11 @@
-import { init } from 'snabbdom/init'
-import { classModule } from 'snabbdom/modules/class'
-import { propsModule } from 'snabbdom/modules/props'
-import { styleModule } from 'snabbdom/modules/style'
-import { eventListenersModule } from 'snabbdom/modules/eventlisteners'
-import { h } from 'snabbdom/h' // helper function for creating vnodes
+import h from './mysnabbdom/h.js'
 
-// 创建出patch函数
-const patch = init([classModule, propsModule, styleModule, eventListenersModule])
-
-// 创建虚拟节点
-const myVnode1 = h('a', {
-  props: {
-    href: 'http://www.baidu.com',
-    target: '_blank'
-  }
-}, '百度')
-console.log(myVnode1)
-
-const myVnode2 = h('div', {
-  class: {
-    'box': true
-  }
-}, '我是一个盒子')
-
-const myVnode3 = h('ul', [
-  h('li', '苹果'),
-  h('li', '西瓜'),
-  h('li', '香蕉'),
-  h('li', '鸭梨')
+var myVnode1 = h('ul', {}, [
+  h('li', {}, '哈哈'),
+  h('li', {}, [
+    h('li', {}, '哈哈'),
+    h('li', {}, '嘻嘻')
+  ]),
+  h('li', {}, h('span', {}, '文字'))
 ])
-
-// 让虚拟节点上树
-const container = document.getElementById('container')
-patch(container, myVnode3)
+console.log(myVnode1)
---
+++ study-snabbdom/src/mysnabbdom/vnode.js
@@ -0,0 +1,6 @@
+// 函数的功能 就是把传入的五个参数组合成对象返回
+export default function (sel, data, children, text, elm) {
+  return {
+    sel, data, children, text, elm
+  }
+}
